138a8c0049ad019880b113d5887fcae3fb7fb3fb
KodeKing22 <jonkylemars@yahoo.com>
2022-06-24 15:10:34 -0500

    Created URL's File in Comments App + Added Replay Serializer toSerializer File.

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-from .models import Comments
+from .models import Comments, Replay
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
         fields = ['id', 'video_id', 'text', 'likes', 'dislikes' 'user_id']
+        depth = 1
+
+class ReplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Replay
+        fields = ['id', 'user', 'text', 'comment']
         depth = 1
