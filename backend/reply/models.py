7b8a2ba6cac9add914a9a2d91b06bf4f14db0241
KodeKing22 <jonkylemars@yahoo.com>
2022-08-27 16:53:17 -0500

    BackEnd had a few bugs that needed to be fixed to keep it from crashing: Import Comments to Reply and Importing Authentication Models.

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -1,8 +1,12 @@
+
 from django.db import models
 from django.contrib.auth.models import User
+from comments.models import Comments
+from authentication.models import User
 
 # Create your models here.
+
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comments, on_delete=models.CASCADE )
     text = models.CharField(max_length=100)
